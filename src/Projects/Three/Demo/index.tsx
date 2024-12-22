f1f0f63d410cef2ead55accc95456a68a065180c
Youri Reijne <youri.reijne@codegrade.com>
2024-12-22 22:25:53 +0100

    Add demo project for three js
    We use a simple demo project, which is lazy loaded like all projects, to check
    if this structure will work in the deployed app.

---
+++ src/Projects/Three/Demo/index.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+
+const DemoScene: React.FC = () => {
+    const mountRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000,
+        );
+        const renderer = new THREE.WebGLRenderer();
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        const currentMount = mountRef.current;
+        currentMount?.appendChild(renderer.domElement);
+
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+
+        camera.position.z = 5;
+
+        const animate = () => {
+            requestAnimationFrame(animate);
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+            renderer.render(scene, camera);
+        };
+        animate();
+
+        return () => {
+            currentMount?.removeChild(renderer.domElement);
+            renderer.dispose();
+        };
+    }, []);
+
+    return <div ref={mountRef}></div>;
+};
+
+export default DemoScene;
