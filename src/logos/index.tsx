9827e4ee455e32ac69dc22fe9522bcf4162b24c9
Youri Reijne <youri.reijne@codegrade.com>
2024-12-23 00:31:26 +0100

    Revert "BROKEN: Attempt to make random smileys on home page"
    This reverts commit b9c2c56811084d1152717dedd7fffa22e7711371.

--- src/logos/index.tsx
+++ src/logos/index.tsx
@@ -1,98 +1,52 @@
-import email from './email.svg';
-import faceLogo from './faceLogo.svg';
-import github from './github.svg';
-import linkedIn from './linkedIn.svg';
+import faceLogo from './faceLogo.svg'
+import github from './github.svg'
+import linkedIn from './linkedIn.svg'
+import email from './email.svg'
 
-import './logos.css';
+import './logos.css'
 
-interface SmileyCombo {
-    left: string;
-    right: string;
-}
-
-const WAVE = '👋';
-const THUMBS_UP = '👍';
-const ROCK_HAND = '🤘';
-const POINT = '🫵';
-
-// const SMILEYS = [ , , , '', '', ''];
-const SMILEYS: SmileyCombo[] = [
-    { left: WAVE, right: '😄' },
-    { left: '😉', right: THUMBS_UP },
-    { left: WAVE, right: '😶‍🌫️' },
-    { left: POINT, right: '🤨' },
-    { left: WAVE, right: '😊' },
-    { left: WAVE, right: '😁' },
-    { left: ROCK_HAND, right: '😜' },
-];
-// const EXTRA = ['☕', '✌', '✍'];
-// ( • ᴗ - )
 export function FaceLogo() {
-    const renderBlackWhite = Math.random() < 0.5;
-
-    const renderBlackWhiteLogo = () => (
-        <div className="face-logo-black-white">
-            <div className="unicode">✍</div>
-            <img src={faceLogo} className="svg" alt="logo" />
-            <div className="unicode">☕</div>
-        </div>
-    );
-
-    const renderColoredSmiley = () => {
-        const randomSmiley = SMILEYS[Math.floor(Math.random() * SMILEYS.length)];
-        return (
-            <div className="face-logo-color">
-                <div className="unicode">{randomSmiley.left}</div>
-                <div className="unicode">{randomSmiley.right}</div>
-            </div>
-        );
-    };
-
-    return (
-        <div className="face-logo-wrapper">
-            {renderBlackWhite ? renderBlackWhiteLogo() : renderColoredSmiley()}
-        </div>
-    );
+  return <img src={faceLogo} className="Face-logo" alt="logo" />
 }
 
 export function GithubLogo() {
-    return (
-        <a
-            href="https://github.com/reijne"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="social-link"
-        >
-            <img src={github} alt="GitHub" className="social-logo" />
-            <span>reijne</span>
-        </a>
-    );
+  return (
+    <a
+      href="https://github.com/reijne"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="social-link"
+    >
+      <img src={github} alt="GitHub" className="social-logo" />
+      <span>reijne</span>
+    </a>
+  )
 }
 
 export function LinkedInLogo() {
-    return (
-        <a
-            href="https://www.linkedin.com/in/youri-reijne/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="social-link"
-        >
-            <img src={linkedIn} alt="LinkedIn" className="social-logo" />
-            <span>youri-reijne</span>
-        </a>
-    );
+  return (
+    <a
+      href="https://www.linkedin.com/in/youri-reijne/"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="social-link"
+    >
+      <img src={linkedIn} alt="LinkedIn" className="social-logo" />
+      <span>youri-reijne</span>
+    </a>
+  )
 }
 
 export function EmailLogo() {
-    return (
-        <a
-            href="mailto:y.reijne@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="social-link"
-        >
-            <img src={email} alt="Email" className="social-logo" />
-            <span>y.reijne@gmail.com</span>
-        </a>
-    );
+  return (
+    <a
+      href="mailto:y.reijne@gmail.com"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="social-link"
+    >
+      <img src={email} alt="Email" className="social-logo" />
+      <span>y.reijne@gmail.com</span>
+    </a>
+  )
 }
